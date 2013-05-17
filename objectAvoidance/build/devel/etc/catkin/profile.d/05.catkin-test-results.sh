3abb2a6c034aea25ecc230c405090533759f650f
Bastion Fennell <bastionfennell@utexas.edu>
2013-05-17 12:30:19 -0500

    Removed unneeded files

--- objectAvoidance/build/devel/etc/catkin/profile.d/05.catkin-test-results.sh
+++
@@ -1,4 +0,0 @@
-# generated from catkin/cmake/env-hooks/05.catkin-test-results.sh.develspace.in
-
-export CATKIN_TEST_RESULTS_DIR="/nishome/ocroomes/ros/rosbuild_ws/class-code/objectAvoidance/build/test_results"
-export ROS_TEST_RESULTS_DIR="$CATKIN_TEST_RESULTS_DIR"
